5d8ebffb9a56e0d89ab86fc1092cbf151fe77ef7
Yawhou <54216858+Yawhou@users.noreply.github.com>
2024-04-15 12:24:12 +0800

    Reapply "Merge branch 'main' into authorisation"
    This reverts commit 19b6468282e4cd9d1ca62222879535e15a925963.

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -22,11 +22,19 @@ class OrderController extends Controller
      */
     public function index()
     {
+        // Retrieve paginated orders with associated products
         $data['orders']=OrderProduct::with('order','product')->paginate(10);
 
+        // Return view with orders data
         return view('order.ordermanage', $data);
     }
 
+    /**
+     * Remove the specified order and its associated products.
+     *
+     * @param  int  $id
+     * @return RedirectResponse
+     */
     public function destroy($id): RedirectResponse
     {
         try {
@@ -34,15 +42,18 @@ class OrderController extends Controller
             $data = OrderProduct::where('order_id',$id)->get();
             $order_data = Order::where('id', $id)->get();
 
+            // Delete the order
             foreach ($data as $datum)
             {$datum->delete();}
             foreach ($order_data as $datum)
             {$datum->delete();}
+            // Flash success message and redirect to order index
             session()->flash('message', 'Deleted!');
             session()->flash('type', 'success');
             return redirect()->route('orders.index');
         }
         catch(Exception $e){
+            // Flash error message and redirect back in case of exception
             session()->flash('message',$e->getMessage());
             session()->flash('type','danger');
 
